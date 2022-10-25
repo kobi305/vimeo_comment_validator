a37f038f94c0c4b2f8d3ec06a155790b081f1483
kobi sharvit <kobisrvit@gmail.com>
2022-10-25 12:50:20 +0300

    add validate to comment object

--- comment.py
+++ comment.py
@@ -1,11 +1,5 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Comment:
-    video: str
-    text: str
-    id: str = None
+from dataclasses import dataclass, fields, Field
+from typing import Tuple
 
 
 class InvalidCommentException(Exception):
@@ -18,3 +12,18 @@ class InvalidCommentException(Exception):
                 self.message += f'{key} '
 
         super().__init__(self.message)
+
+
+@dataclass
+class Comment:
+    video: str = None
+    text: str = None
+    id: str = None
+    is_verified: bool = False
+
+    def validate(self):
+        cls_fields: Tuple[Field, ...] = fields(self.__class__)
+
+        for field in cls_fields:
+            if not getattr(self, field.name) and field.name != 'is_verified':
+                raise InvalidCommentException(self.__dict__)
